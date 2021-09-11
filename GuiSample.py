df5bcb77d1eab61671be415040fb6416bada749f
dohhea <50563398+dohhea@users.noreply.github.com>
2021-09-11 21:01:36 +0900

    minor comment changes

--- GuiSample.py
+++ GuiSample.py
@@ -2,14 +2,14 @@
 
 from tkinter import *
 from tkinter.ttk import *
-import tkinter											# tkinter.CURRENT를 사용하기 위해 별로도 import 하여야 함
+import tkinter												# tkinter.CURRENT를 사용하기 위해 별로도 import 하여야 함
 
-class MyFrame(Frame):									# 간단한 상속 활용
+class MyFrame(Frame):										# 간단한 상속 활용
 
 	def __init__(self, master):								# 생성자
 		Frame.__init__(self, master)						# 상속받은 프레임을 master 로 연결
 
-		self.buttonSave = Button()							# 나중에 쓰일 버튼을 정의해 주고
+		self.saveButton = Button()							# 나중에 쓰일 버튼을 정의해 주고
 		def onClick():										# 이것이 눌릴때 실행할 핸들러를 정의
 			str1=entryName.get()							
 			str2=entryComp.get()
@@ -22,12 +22,12 @@ class MyFrame(Frame):									# 간단한 상속 활용
 
 		# 성명
 		frame1 = Frame(self)								# 상위 프레임에 들어갈 서브 프레임생성
-		frame1.pack(fill=X)								# 수평방향으로 채워짐
+		frame1.pack(fill=X)									# 수평방향으로 채워짐
 
 		lblName = Label(frame1, text="성명", width=10)		# 레이블 1개
 		lblName.pack(side=LEFT, padx=10, pady=10)
 
-		entryName = Entry(frame1)						# 엔트리 1 개
+		entryName = Entry(frame1)							# 엔트리 1 개
 		entryName.pack(fill=X, padx=10, expand=True)	
 
 		# 회사
@@ -59,8 +59,8 @@ class MyFrame(Frame):									# 간단한 상속 활용
 def main():
     root = Tk()
     root.geometry("600x550+100+100")							# 프레임의 크기와 위치설정
-    app = MyFrame(root)										# 프레임 생성
-    root.mainloop()											# 프레임이 화면에 머물도록 함
+    app = MyFrame(root)											# 프레임 생성
+    root.mainloop()												# 프레임이 화면에 머물도록 함
  
  
 if __name__ == '__main__':
