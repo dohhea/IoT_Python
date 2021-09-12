86732ae45751b98350dda463ddf2f24df4e06cac
Dongik Oh <dohhea@gmail.com>
2021-09-12 21:40:49 +0900

    주사위 게임 구현

--- exer8.py
+++ exer8.py
@@ -11,3 +11,51 @@
 # •
 # 경기가 종료되면 그때까지의 승패정보를 출력해 줍니다
 
+import random as rand
+class Die:
+    def __init__(self, max=6):
+        self.MAX = max              # 만드는 주사위 면의 수
+	
+    # 주사위 던지기
+    def roll(self):
+        self.faceValue = int(rand.random()*self.MAX)+1
+	
+    def getFaceValue(self):
+        return self.faceValue
+	
+    def toString(self):
+        return str(self.faceValue)
+
+def main():
+    userWin= 0
+    comWin = 0
+    draw= 0
+    user = Die()                        # 사용자의 주사위
+    com = Die()		                    # 컴퓨터의 주사위
+
+    # 게임의 진행
+    while (True):
+		# 게임 한번하고
+        user.roll()
+        com.roll()
+        print("나는 : "+ user.toString() + ", 컴은 : " + com.toString())
+        if (user.getFaceValue() == com.getFaceValue()):
+            print("비겼습니다.")
+            draw +=1
+        elif (user.getFaceValue() > com.getFaceValue()):
+            print("내가 이겼습니다.")
+            userWin+=1
+        else:
+            print("컴이 이겼습니다.")
+            comWin+=1
+		
+        # 계속 진행할지의 여부 파악
+        cont=input("계속할까요? (y/n) ")
+        if cont=="n" or cont=="N":
+            break
+
+    # 게임 총 결과 출력
+    print ("총 "+ str(userWin + comWin + draw) + "경기 중 user " + str(userWin) + "회 승, COM "+ str(comWin) + "회 승, 무승부 " + str(draw) + "회" )
+
+
+main()
